1e9fa780a010d5b30e2ee8f61722ae2ba0d2369f
Ross Parker <>
2024-11-01 14:02:03 +0800

    v1.1.02

--- ATL/manifest.php
+++ ATL/manifest.php
@@ -27,7 +27,7 @@ $description = 'The ATL module allows schools to run a program of Approaches To 
 $entryURL = 'atl_write.php';
 $type = 'Additional';
 $category = 'Assess';
-$version = '1.6.01';
+$version = '1.6.02';
 $author = "Gibbon Foundation";
 $url = "https://gibbonedu.org";
 
